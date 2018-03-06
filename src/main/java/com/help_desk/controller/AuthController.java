4813203b45b16019b09dbb3cec3194fee8162e6b
SpardaRus <ale-altair@yandex.ru>
2018-03-06 22:50:14 +0400

    add css

--- src/main/java/com/help_desk/controller/AuthController.java
+++ src/main/java/com/help_desk/controller/AuthController.java
@@ -51,12 +51,6 @@ public class AuthController {
 
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
-        if (error != null)
-            model.addAttribute("error", "Your username and password is invalid.");
-
-        if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
-
         return "auth/login";
     }
 
